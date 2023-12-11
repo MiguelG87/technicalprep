409cb7e237352d7dfc6a09533f87bd21f30b3e9a
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-12-10 21:41:04 -0600

    Example 15

--- src/bigo/ExamplesBigO.java
+++ src/bigo/ExamplesBigO.java
@@ -146,14 +146,30 @@ public class ExamplesBigO {
     // }
 
     //Example 14
-    void allFib(int n) {
-        for (int i = 0; i <= n; i++) {
-            System.out.println(i + ": " + fib(i));
+    // void allFib(int n) {
+    //     for (int i = 0; i <= n; i++) {
+    //         System.out.println(i + ": " + fib(i));
+    //     }
+    // }
+    // int fib(int n) {
+    //     if (n <= 0) return 0;
+    //     else if (n == 1) return 1;
+    //     return fib(n - 1) + fib(n - 2);
+    // }
+
+    //Example 15
+        void allFib(int n) {
+            int[] memo = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                System.out.println(i + ": " + fib(i, memo));
+            }
         }
-    }
-    int fib(int n) {
-        if (n <= 0) return 0;
-        else if (n == 1) return 1;
-        return fib(n - 1) + fib(n - 2);
-    }
+        int fib(int n, int[] memo) {
+            if (n <= 0) return 0;
+            else if (n == 1) return 1;
+            else if (memo[n] > 0) return memo[n];
+
+            memo[n] = fib(n - 1, memo) + fib(n - 2, nemo);
+            return memo[n];
+        }
 }
