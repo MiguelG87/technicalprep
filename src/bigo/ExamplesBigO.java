cbba1f276c4928d9f97e8744e48d1d580df901d9
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-12-11 22:27:00 -0600

    Example 16

--- src/bigo/ExamplesBigO.java
+++ src/bigo/ExamplesBigO.java
@@ -169,7 +169,21 @@ public class ExamplesBigO {
             else if (n == 1) return 1;
             else if (memo[n] > 0) return memo[n];
 
-            memo[n] = fib(n - 1, memo) + fib(n - 2, nemo);
+            memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
             return memo[n];
+        }
+
+        int powersOf2(int n) {
+            if (n < 1) {
+                return 0;
+            } else if (n == 1) {
+                System.out.println(1);
+                return 1;
+            } else {
+                int prev = powersOf2(n / 2);
+                int curr = prev * 2;
+                System.out.println(curr);
+                return curr;
+            }
         }
 }
