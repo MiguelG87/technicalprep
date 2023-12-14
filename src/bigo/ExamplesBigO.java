42e1012f339809fa23485db24e86d29cfc9f1c20
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-12-13 23:00:18 -0600

    Additional Example 2

--- src/bigo/ExamplesBigO.java
+++ src/bigo/ExamplesBigO.java
@@ -199,4 +199,16 @@ public class ExamplesBigO {
             }
             return sum;
         }
+
+        //Example VI.2
+        int power(int a, int b) {
+            if (b < 0) {
+                return 0; // error
+            } else if (b == 0) {
+                return 1;
+            } else {
+                return a * power(a, b - 1);
+            }
+
+        }
 }
